85f2025ad753258542e1125d08ac63a4385b53c0
劉顥權 Liu Haoquan <haoquan291@gmail.com>
2021-09-26 22:42:10 +0800

    修正特殊班級無法查詢的問題

--- classList.py
+++ classList.py
@@ -3,14 +3,29 @@ import requests
 from bs4 import BeautifulSoup
 
 # 上課節數（可能因暑輔而有所影響，可自行調整使程式正常運行）
-classNum = 6 # 暑輔
-# classNum = 8 # 平常
+# classNum = 6 # 暑輔
+classNum = 8 # 正常
 
 # 學期代碼（基本上不需要更換）
-semesterId = "1101101"
+semesterId = "110110"
 
-classId = str(input('請輸入想查詢的班級：'))
-r = requests.get("http://acdm3.tcssh.tc.edu.tw/csv3_web/SF4.ASP?CLA_NO=" + semesterId + classId)
+# 班級群代碼（請勿更改）
+groupId = "1"
+
+# 班級代碼
+classId = str()
+while(len(classId) != 3):
+    classId = input('請輸入想查詢的班級：')
+
+# 判斷是否為特殊班級
+if classId[1:3] == "21":
+    groupId = "6"
+elif classId[1:3] == "22":
+    groupId = "7"
+elif classId[1:3] == "23":
+    groupId = "2"
+
+r = requests.get("https://acdm3.tcssh.tc.edu.tw/csv3_web/SF4.ASP?CLA_NO=" + semesterId + groupId + classId)
 r.encoding = 'big5-hkscs'
 r = r.content
 soup = BeautifulSoup(r, "lxml")
