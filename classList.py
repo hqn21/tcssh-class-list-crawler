b0750e6561a068b1f0d2cbe752b5955bd04bace8
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2022-03-18 16:18:56 +0800

    增加日期判斷

--- classList.py
+++ classList.py
@@ -1,13 +1,23 @@
 # coding=utf-8
 import requests
+import datetime
 from bs4 import BeautifulSoup
 
+today = datetime.datetime.today()
+year = today.year
+month = today.month
+
 # 上課節數（可能因暑輔而有所影響，可自行調整使程式正常運行）
-# classNum = 6 # 暑輔
-classNum = 8 # 正常
+if(month == 7 or month == 8):
+    classNum = 6 # 暑輔 
+else:
+    classNum = 8 # 正常
 
 # 學期代碼（基本上不需要更換）
-semesterId = "110110"
+if(month >= 7):
+    semesterId = str(year - 1911)
+else:
+    semesterId = str(year - 1912)
 
 # 班級群代碼（請勿更改）
 groupId = "1"
@@ -25,14 +35,14 @@ elif classId[1:3] == "22":
 elif classId[1:3] == "23":
     groupId = "2"
 
-r = requests.get("https://acdm3.tcssh.tc.edu.tw/csv3_web/SF4.ASP?CLA_NO=" + semesterId + groupId + classId)
+r = requests.get("https://acdm3.tcssh.tc.edu.tw/csv3_web/SF4.ASP?CLA_NO=" + semesterId + semesterId + groupId + classId, headers={'Cache-Control': 'no-cache'})
 r.encoding = 'big5-hkscs'
 r = r.content
 soup = BeautifulSoup(r, "lxml")
 texts = soup.find_all("font")
-i = 0
 
 print(' ')
+
 for i in range(1, 6):
     num = i + 7
     print(texts[i].string)
